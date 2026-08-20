bf7530beaaf53c61ea9c7df42e6d34e681cf8e4a
quangvo2007ne <quangvo2007ne@users.noreply.github.com>
2026-08-20 17:13:59 +0700

    Fix undeclared identifier LQBypassHelper

--- LQBypass/LQBypass.m
+++ LQBypass/LQBypass.m
@@ -13,75 +13,11 @@
 static id g_modControllerInstance = nil;
 
 // -------------------------------------------------------------------------
-// 1. Helper tìm Image Base của AWSS3
-// -------------------------------------------------------------------------
-uintptr_t get_awss3_base_slide(void) {
-    uint32_t count = _dyld_image_count();
-    for (uint32_t i = 0; i < count; i++) {
-        const char *name = _dyld_get_image_name(i);
-        if (name && strstr(name, "AWSS3.framework/AWSS3")) {
-            return (uintptr_t)_dyld_get_image_vmaddr_slide(i);
-        }
-    }
-    return 0;
-}
-
-// -------------------------------------------------------------------------
-// 2. Hàm Kích Hoạt Menu Mod & Vẽ Nút Nổi
-// -------------------------------------------------------------------------
-void bootstrap_mod_menu(void) {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        NSLog(@"[LQBypass] Bắt đầu khởi tạo Menu Mod...");
-        
-        Class cls = objc_getClass("tXGBBDJNKKzPYcSGmlav");
-        if (cls) {
-            // [cls new] hoặc [[cls alloc] init]
-            g_modControllerInstance = [[cls alloc] init];
-            NSLog(@"[LQBypass] Đã tạo instance tXGBBDJNKKzPYcSGmlav: %@", g_modControllerInstance);
-            
-            // Lưu vào BSS cache nếu có slide
-            uintptr_t slide = get_awss3_base_slide();
-            if (slide > 0) {
-                uintptr_t *cache_ptr = (uintptr_t *)(slide + 0x36BDFD0);
-                *cache_ptr = (uintptr_t)g_modControllerInstance;
-                NSLog(@"[LQBypass] Đã ghi instance vào BSS cache @ %p", cache_ptr);
-            }
-            
-            // Kích hoạt tạo cử chỉ và vẽ nút tròn nổi
-            if ([g_modControllerInstance respondsToSelector:@selector(initTapGes)]) {
-                [g_modControllerInstance performSelector:@selector(initTapGes)];
-                NSLog(@"[LQBypass] Đã gọi [initTapGes] thành công!");
-            } else if ([g_modControllerInstance respondsToSelector:@selector(setupFloatingToggleButtons)]) {
-                [g_modControllerInstance performSelector:@selector(setupFloatingToggleButtons)];
-                NSLog(@"[LQBypass] Đã gọi [setupFloatingToggleButtons] thành công!");
-            }
-        } else {
-            NSLog(@"[LQBypass] CẢNH BÁO: Không tìm thấy class tXGBBDJNKKzPYcSGmlav");
-        }
-        
-        // Thêm cử chỉ dự phòng: Gõ 2 ngón 2 lần (2-finger double tap) để ép bật/tắt Menu ImGui
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
-            if (!keyWindow && [[UIApplication sharedApplication].windows count] > 0) {
-                keyWindow = [UIApplication sharedApplication].windows.firstObject;
-            }
-            if (keyWindow) {
-                UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:[LQBypassHelper class] action:@selector(toggleImGuiMenu)];
-                tap.numberOfTouchesRequired = 2;
-                tap.numberOfTapsRequired = 2;
-                [keyWindow addGestureRecognizer:tap];
-                NSLog(@"[LQBypass] Đã thêm cử chỉ dự phòng (chạm 2 lần bằng 2 ngón) vào UIWindow!");
-            }
-        });
-    });
-}
-
-// -------------------------------------------------------------------------
-// 3. Class Helper cho Gesture & Swizzling
+// 1. Khai báo Class Helper trước để tránh lỗi undeclared identifier
 // -------------------------------------------------------------------------
 @interface LQBypassHelper : NSObject
 + (void)toggleImGuiMenu;
++ (void)bootstrapModMenu;
 @end
 
 @implementation LQBypassHelper
@@ -102,16 +38,81 @@ void bootstrap_mod_menu(void) {
     }
 }
 
++ (void)bootstrapModMenu {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"[LQBypass] Bắt đầu khởi tạo Menu Mod...");
+        
+        Class cls = objc_getClass("tXGBBDJNKKzPYcSGmlav");
+        if (cls) {
+            g_modControllerInstance = [[cls alloc] init];
+            NSLog(@"[LQBypass] Đã tạo instance tXGBBDJNKKzPYcSGmlav: %@", g_modControllerInstance);
+            
+            // Tìm image slide của AWSS3
+            uintptr_t slide = 0;
+            uint32_t count = _dyld_image_count();
+            for (uint32_t i = 0; i < count; i++) {
+                const char *name = _dyld_get_image_name(i);
+                if (name && strstr(name, "AWSS3.framework/AWSS3")) {
+                    slide = (uintptr_t)_dyld_get_image_vmaddr_slide(i);
+                    break;
+                }
+            }
+            
+            if (slide > 0) {
+                uintptr_t *cache_ptr = (uintptr_t *)(slide + 0x36BDFD0);
+                *cache_ptr = (uintptr_t)g_modControllerInstance;
+                NSLog(@"[LQBypass] Đã ghi instance vào BSS cache @ %p", cache_ptr);
+            }
+            
+            // Kích hoạt tạo cử chỉ và vẽ nút tròn nổi
+            if ([g_modControllerInstance respondsToSelector:@selector(initTapGes)]) {
+                [g_modControllerInstance performSelector:@selector(initTapGes)];
+                NSLog(@"[LQBypass] Đã gọi [initTapGes] thành công!");
+            } else if ([g_modControllerInstance respondsToSelector:@selector(setupFloatingToggleButtons)]) {
+                [g_modControllerInstance performSelector:@selector(setupFloatingToggleButtons)];
+                NSLog(@"[LQBypass] Đã gọi [setupFloatingToggleButtons] thành công!");
+            }
+        } else {
+            NSLog(@"[LQBypass] CẢNH BÁO: Không tìm thấy class tXGBBDJNKKzPYcSGmlav");
+        }
+        
+        // Thêm cử chỉ dự phòng: Gõ 2 ngón 2 lần (2-finger double tap) để ép bật/tắt Menu ImGui
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            UIWindow *window = nil;
+            if (@available(iOS 13.0, *)) {
+                for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                    if (scene.activationState == UISceneActivationStateForegroundActive) {
+                        for (UIWindow *w in scene.windows) {
+                            if (w.isKeyWindow) { window = w; break; }
+                        }
+                    }
+                    if (window) break;
+                }
+            }
+            if (!window && [[UIApplication sharedApplication].windows count] > 0) {
+                window = [UIApplication sharedApplication].windows.firstObject;
+            }
+            if (window) {
+                UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:[LQBypassHelper class] action:@selector(toggleImGuiMenu)];
+                tap.numberOfTouchesRequired = 2;
+                tap.numberOfTapsRequired = 2;
+                [window addGestureRecognizer:tap];
+                NSLog(@"[LQBypass] Đã thêm cử chỉ dự phòng (chạm 2 lần bằng 2 ngón) vào UIWindow!");
+            }
+        });
+    });
+}
+
 @end
 
 // -------------------------------------------------------------------------
-// 4. Hook vô hiệu hoá Dialog Key & UDID (ASStatusView)
+// 2. Hook vô hiệu hoá Dialog Key & UDID (ASStatusView)
 // -------------------------------------------------------------------------
 static void hook_status_view(void) {
     Class statusViewCls = objc_getClass("ASStatusView");
     if (!statusViewCls) return;
     
-    // Tạo block giả lập hàm dismiss/no-op
     id dummyBlock = ^(id self, ...) {
         NSLog(@"[LQBypass] Chặn thành công Popup Key / UDID!");
     };
@@ -139,7 +140,7 @@ static void hook_status_view(void) {
 }
 
 // -------------------------------------------------------------------------
-// 5. Entry point khi Dylib được nạp vào App
+// 3. Entry point khi Dylib được nạp vào App
 // -------------------------------------------------------------------------
 __attribute__((constructor))
 static void lq_bypass_init(void) {
@@ -157,12 +158,12 @@ static void lq_bypass_init(void) {
                                                   usingBlock:^(NSNotification * _Nonnull note) {
         NSLog(@"[LQBypass] Nhận thông báo UIApplicationDidFinishLaunchingNotification!");
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            bootstrap_mod_menu();
+            [LQBypassHelper bootstrapModMenu];
         });
     }];
     
     // 3. Dự phòng nếu notification đã trôi qua
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        bootstrap_mod_menu();
+        [LQBypassHelper bootstrapModMenu];
     });
 }
